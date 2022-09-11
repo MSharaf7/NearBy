37a95101edffc23f6b25e203c0465ed950a6c854
MSharaf7 <72823525+MSharaf7@users.noreply.github.com>
2022-09-11 04:36:25 -0400

    Add TODO for peer

---
+++ NearBy/Managers/LocationManager.swift
@@ -0,0 +1,10 @@
+//
+//  LocationManager.swift
+//  NearBy
+//
+//  Created by Marwan Sharaf on 9/11/22.
+//
+
+// TODO: Implement LocationManager to request location info from user
+
+import Foundation
