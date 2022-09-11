45cb5e10630de9812d138adb963ca6e8b4cdea57
MSharaf7 <72823525+MSharaf7@users.noreply.github.com>
2022-09-11 04:28:42 -0400

    Initial Commit

---
+++ NearBy/NearByApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearByApp.swift
+//  NearBy
+//
+//  Created by Marwan Sharaf on 9/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NearByApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NearBy/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NearBy
+//
+//  Created by Marwan Sharaf on 9/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
